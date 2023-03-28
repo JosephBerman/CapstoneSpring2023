32bb7b6c9244ef2200b0779406bfb55fe737e403
JosephBerman <berman.jos@northeastern.edu>
2023-03-28 13:39:41 -0400

    added mlx driver from existing repo

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -59,6 +59,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("/Users/waffles/Documents/Capstone/CapstoneSpring2023/include/mlx90640-RPI-Pico/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("/Users/waffles/Documents/Capstone/CapstoneSpring2023/src/cmake_install.cmake")
 endif()
 
